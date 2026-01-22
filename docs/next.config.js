42eb57bf2093731051fc6b7b97751825417a0679
ddux <dusan.stanojevic.cs@gmail.com>
2026-01-21 23:27:09 -0600

    traceway docs started

---
+++ docs/next.config.js
@@ -0,0 +1,16 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  defaultShowCopyCode: true,
+  flexsearch: {
+    codeblocks: true
+  }
+})
+
+module.exports = withNextra({
+  output: 'export',
+  basePath: '/docs',
+  images: {
+    unoptimized: true
+  }
+})

a593e446a323e709efc27e13b864224d0c7e2b46
ddux <dusan.stanojevic.cs@gmail.com>
2026-01-22 00:47:01 -0600

    assets cleanup

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Traceway",
   description: "Telemetry & Issue Tracking for Golang",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({

30d131f2161d5729571bd33ab0285b3738e142a6
ddux <dusan.stanojevic.cs@gmail.com>
2026-01-21 20:56:18 -0600

    progress

--- backend/app/migrations/ch/0009_insert_default_project.up.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO projects (id, name, token, created_at) VALUES (
-    '00000000-0000-0000-0000-000000000001',
-    'Default Project',
-    'default_token_change_me',
-    now()
-)
--- backend/app/migrations/pg/0003_insert_default_project.up.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO projects (id, name, token, framework, created_at)
-VALUES ('00000000-0000-0000-0000-000000000001', 'Default Project', 'default_token_change_me', 'custom', NOW())
-ON CONFLICT (id) DO NOTHING

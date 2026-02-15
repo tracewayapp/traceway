55dbd51ee086162c297b2cbb93fde0b2fda35129
ddux <dusan.stanojevic.cs@gmail.com>
2026-02-15 17:12:27 -0600

    bugfix: otel project validation

--- backend/app/controllers/auth.controller.go
+++ backend/app/controllers/auth.controller.go
@@ -154,7 +154,7 @@ func (a authController) Register(c *gin.Context) {
 
 	if !validFrameworks[request.Framework] {
 		traceway.CaptureMessage("Invalid framework received during registration: " + request.Framework)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Framework must be one of: gin, fiber, chi, fasthttp, stdlib, custom, react, svelte, vuejs, nextjs, nestjs, express, remix"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Framework must be one of: gin, fiber, chi, fasthttp, stdlib, custom, react, svelte, vuejs, nextjs, nestjs, express, remix, opentelemetry"})
 		return
 	}
 
